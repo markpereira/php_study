563c0bb0fe0d8966c39d00b6dd685a764ccf807c
markpereira <mark.pereira@xtra.co.nz>
2014-07-29 15:48:06 +1000

    second commit

---
+++ sandbox/helloworld.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Hello World</title>
+	</head>
+	<body>
+		<?php 
+		//single line comment
+		# or like this (less common)
+		/* Double line comments are written like this
+		so you can keep typing
+		and typing
+		*/
+		?>
+		<?php echo "Hello World"; ?><br />
+		<?php echo "Hello" . " World"; ?><br />
+		<?php echo 2 + 3; ?>
+	</body>
+</html>		
