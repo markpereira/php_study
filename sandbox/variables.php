53a30e14ca0131ac75af252f0a08452d9302e7b7
markpereira <mark.pereira@xtra.co.nz>
2014-07-29 16:23:15 +1000

    string functions

--- sandbox/variables.php
+++ sandbox/variables.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>untitled</title>
+		<title>Variables</title>
 	</head>
 	<body>
+		<?php
+
+		$var1 = 10;
+		echo $var1;
+
+		echo "<br />";
+
+		$var1 = 100;
+		echo $var1;
+
+		echo "<br />";
+
+		$var2 = "Hello World";
+		echo $var2;
+		?>
 	</body>
 </html>		
-<?php echo "Hello world"; ?>
