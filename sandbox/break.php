99a7090a3c7e8a2b82118181d32894092b4f404b
markpereira <mark.pereira@xtra.co.nz>
2014-07-30 23:11:29 +1000

    modify break.php

--- sandbox/break.php
+++ sandbox/break.php
@@ -4,13 +4,25 @@
 		<title>Break</title>
 	</head>
 	<body>
-		<?php 
+		<?php // example of break
 			for ($count=0; $count <= 10; $count++) {
 				if ($count == 5) {
-					continue;
+					break;
 				}
 				echo $count . ", ";
 			}
 		?>
+		<br />
+		<?php // loop inside a loop using break
+			
+			for ($i=0; $i<=5; $i++) {
+				if ($i % 2 == 0) { continue(1); }
+				for ($k=0; $k<=5; $k++) {
+					if ($k == 3) { break(1); }
+					echo $i . "_" . $k . "<br />";
+				}
+			}
+		?>
+		<br />
 	</body>
 </html>		
