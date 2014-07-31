2151d2e9b9ace072384823bd6491958f423b2b6e
markpereira <mark.pereira@xtra.co.nz>
2014-07-31 14:52:04 +1000

    urlencode

--- sandbox/first_page.php
+++ sandbox/first_page.php
@@ -7,7 +7,8 @@
 
 		<?php $link_name = "Second Page"; ?>
 		<?php $id = 5; ?>
-		<a href="second_page.php?id=<?php echo $id; ?>"><?php echo $link_name; ?></a>
+		<?php $company = "Johnson & Johnson"; ?>
+		<a href="second_page.php?id=<?php echo $id; ?>&company=<?php echo rawurlencode($company); ?>"><?php echo $link_name; ?></a>
 
 	</body>
 </html>		
--- sandbox/second_page.php
+++ sandbox/second_page.php
@@ -11,5 +11,12 @@
 			echo $id;
 			?>
 	</pre>
+	<br />
+		<?php 
+				//print_r($_GET);
+			$company = $_GET['company'];
+			echo $company;
+			?>
+	<br />
 	</body>
 </html>		
