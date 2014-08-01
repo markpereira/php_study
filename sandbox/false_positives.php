bd51f234fd4d76b9accd0fbba032b065f96da11b
markpereira <mark.pereira@xtra.co.nz>
2014-08-01 10:51:58 +1000

    validations

---
+++ sandbox/false_positives.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>False Positives</title>
+	</head>
+	<body>
+		<?php 
+
+		?>
+	</body>
+</html>		
---
+++ sandbox/validations.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Validations</title>
+	</head>
+	<body>
+		<?php
+
+		// * presence
+		$value = "x";
+		if (!isset($value) || empty($value)) {
+			echo "Validation failed. Nothing entered<br />";
+		}
+
+		// * string length
+		//minimum length
+		$value = "abcde";
+		$min = 3;
+		if (strlen($value) < $min) {
+			echo "Validation failed - min 3 characters.<br />";
+		}
+		// max length
+		$max = 6;
+		if (strlen($value) > $max) {
+			echo "Validation failed - max 6 characters.<br />";	
+		}
+		// * type
+		$value = "22";
+		if (!is_string($value)) {
+			echo "Validation failed. This value is not a string<br />";	
+		}
+		// * inclusion in a set
+		$value = "1";
+		$set = array("1", "2", "3", "4");
+		if (!in_array($value, $set)) {
+			echo "Validation failed - This number is not in the array<br />";	
+		}
+
+		// * uniqueness
+		// uses a database to check uniqueness
+
+
+		// * format
+		//use a regex on a string
+		// preg_match($regex, $subject)
+		if (preg_match("/PHP/", "PHP is Fun.")) {
+			echo "A match was found.";
+		} else {
+			echo "A match was not found";
+		}
+
+		$value = "nobody@nowhere.com";
+		if (!preg_match("/@/", $value)) {
+			echo "<br />Validation failed.<br />";	
+		}
+
+		if (strpos($value, "@") === false){
+			echo "Validation failed.<br />";
+		}
+
+
+
+		?>
+	</body>
+</html>		
